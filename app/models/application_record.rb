ad2d4553ddc9449fce13213b183ded9d55b70d4b
Gael Jerop <gael.jerop@yahoo.com>
2020-12-28 12:59:30 +1100

    changed application model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  skip_before_action :authenticate_user!, only: :home
+  # skip_before_action :authenticate_user!, only: :home
 end
